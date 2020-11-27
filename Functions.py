5ab4f8665501a545baf3d7ef3bb7e0cf18c21b35
ISP.Rucka <ISP.Rucka@gmail.com>
2020-11-27 23:23:01 +0100

    Floyd i rekonstrukcja ścieżki

--- Functions.py
+++ Functions.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import numpy as np
 '''
 funkcje potrzebne do algorytmu
 
@@ -21,8 +22,39 @@ def accept_new(): # sprawdza czy przyjmujemy wygenerowane rozwiazenie
 def calculate_cost(): # liczy koszt (funkcja celu)
     pass
 
-def graf(): # Floyd-Warshall lub djikstra z BFS
-    pass
+def graf(graph): # Floyd-Warshall lub djikstra z BFS
+    #Floyd z opensourca,
+    #number of vertices
+    v = len(graph)
+
+    # path reconstruction matrix
+    p = np.zeros(graph.shape)
+    for i in range(0, v):
+        for j in range(0, v):
+            p[i, j] = i
+    #     if (i != j and graph[i,j] == 0): nwm po co to było, czm nie chcieli używać zerowych u nas chyba useless
+    #        p[i,j] = -30000
+    #         graph[i,j] = 30000 # set zeros to any large number which is bigger then the longest way
+
+    for k in range(0, v):
+        for i in range(0, v):
+            for j in range(0, v):
+                if graph[i, j] > graph[i, k] + graph[k, j]:
+                    graph[i, j] = graph[i, k] + graph[k, j]
+                    p[i, j] = p[k, j]
+    return p
+
+def reconstruct_path(p, i, j,op,k=0):
+    i,j = int(i), int(j)
+    if(i==j and k!=0):
+        op.append(i)
+        print (i,)
+    #elif(p[i,j] == -30000):
+        #print (i,'-',j)
+    else:
+        k = k+1
+        reconstruct_path(p, i, p[i,j],op,k)
+        op.append(j)
 def kochanie_Masia ():
     Maś = jest kochany
     pass
