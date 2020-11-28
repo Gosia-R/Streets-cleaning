37453869256f133fccbafbee7d57c320ae0d082e
ISP.Rucka <ISP.Rucka@gmail.com>
2020-11-28 17:27:45 +0100

    kochanie Masia jest b ważne

--- Functions.py
+++ Functions.py
@@ -6,10 +6,32 @@ funkcje potrzebne do algorytmu
 
 
 '''
+def finding_next_poit_init(i,n,P,A):
+    for k in range(n):
+        if A[i, k] == 1 and ((i, k) and (k, i) not in P):
+            return k
+    return -1
+def finding_next_poit_init_not_OG(i,n,P,A):
+    for k in range(n):
+        if A[i, k] == 1 :
+            return k
+    return -1
 
-def initialize(): # inicjalizacja pierwszego rozwiazania
-    pass
-
+def initialize(Workers): # inicjalizacja pierwszego rozwiazania
+   i = 0
+   while len(P)!= n:
+       for j in range (0,w): # inny range ale nwm jaki
+            next_node = finding_next_poit_init(i, n, P, A)
+            if next_node != -1:
+                pi[j].append(i, next_node)
+                P.append(i, next_node)
+            else:
+                next_node = finding_next_poit_init_not_OG(i,n,P,A)
+                pi[j].append(i, next_node)
+            i = next_node
+   for j in range(0, w):
+       pi[j,:] = reconstruct_path(p,pi[j,-1](1),0,pi[j,:])
+    return pi
 def mutate(): # mutacje, mozliwe ze lepiej zrobic 3 osobne funkcje dla kazdej mutacji
     pass
 
@@ -32,9 +54,9 @@ def graf(graph): # Floyd-Warshall lub djikstra z BFS
     for i in range(0, v):
         for j in range(0, v):
             p[i, j] = i
-    #     if (i != j and graph[i,j] == 0): nwm po co to było, czm nie chcieli używać zerowych u nas chyba useless
-    #        p[i,j] = -30000
-    #         graph[i,j] = 30000 # set zeros to any large number which is bigger then the longest way
+            if graph[i,j] == 0:
+                p[i,j] = -30000
+                graph[i,j] = 30000 # set zeros to any large number which is bigger then the longest way
 
     for k in range(0, v):
         for i in range(0, v):
@@ -44,16 +66,22 @@ def graf(graph): # Floyd-Warshall lub djikstra z BFS
                     p[i, j] = p[k, j]
     return p
 
-def reconstruct_path(p, i, j,op,k=0):
-    i,j = int(i), int(j)
-    if i==j and k!=0:
-        op.append(i)
-    #elif(p[i,j] == -30000):
-        #print (i,'-',j)
+def reconstruct_path(p, i, j,op):
+    if (p[i, j] == i):
+        op = np.append(op, (i, j))
+        return op
+    elif (p[i, j] == -30000):
+        print(i, '-', j)
     else:
-        k = k+1
-        reconstruct_path(p, i, p[i,j],op,k)
-        op.append(j)
+        op = np.append(op, (i, p[i, j]))
+        k = p[i, j]
+        k = int(k)
+        while p[i, k] != i:
+            k = int(k)
+            op = np.append(op, (k, p[i, k]))
+            k = p[i, k]
+            k = int(k)
+        op = np.append(op, (k, j))
     return op
 def kochanie_Masia ():
     Maś = jest kochany
