e9a9b68dab7982a296b80f1e91838d06efc972e4
Maciek <isp.stroinski@gmail.com>
2020-11-28 16:57:12 +0100

    Organize project

--- Worker.py
+++ Worker.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+import numpy as np
 '''
 klasa zawierajaca informacje o pracownikach:
 -ich wydajnosc
 -liste list z ich trasami?
-
-
 '''
+
+class Workers :
+    def __init__(self):
+        self.m = 3 # ilosc pracownikow
+
+        self.wydajnosc = np.array([10, 10, 30])
+
+        self.trasy = np.array([np.array([])]) # wektor zawierajacy kolejno ulice (para wierzcholkow) ktorymi przechodzi kazdy pracownik
